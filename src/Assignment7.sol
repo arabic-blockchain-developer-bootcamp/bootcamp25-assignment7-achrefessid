7ef9e4c22c4b48b788533c0984c7e7fea2e3db37
Achref Essid <essid.developpeur@gmail.com>
2025-03-27 17:40:30 +0100

    assignmant7

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.29;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
